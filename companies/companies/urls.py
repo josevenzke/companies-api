3ccef13f8eeac2210c3051aed0f60411ccb4daff
José Venzke <josevenzkejr@gmail.com>
2021-11-20 05:10:44 -0300

    linting fixes

--- companies/companies/urls.py
+++ companies/companies/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 from api.urls import companies_router
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", include(companies_router.urls)),
 ]
--- companies/companies/wsgi.py
+++ companies/companies/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'companies.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "companies.settings")
 
 application = get_wsgi_application()
